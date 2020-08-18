fa42e2c6ab322a432c141d70d322578d896f1367
Michael Kaip <michael.kaip@student.htw-berlin.de>
2020-08-18 15:42:54 +0200

    Adds Bean Validation to BeerDto. Closes #9.

--- src/main/java/mk/microservices/springframework/msbeerservice/web/model/BeerDto.java
+++ src/main/java/mk/microservices/springframework/msbeerservice/web/model/BeerDto.java
@@ -5,6 +5,11 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Null;
+import javax.validation.constraints.Positive;
+
 import java.math.BigDecimal;
 import java.time.OffsetDateTime;
 import java.util.UUID;
@@ -14,16 +19,27 @@ import java.util.UUID;
 @AllArgsConstructor
 @Builder
 public class BeerDto {
+
+    @Null
     private UUID Id;
+
+    @Null
     private Integer version;
 
+    @Null
     private OffsetDateTime createdDate;
+
+    @Null
     private OffsetDateTime lastModifiedDate;
 
+    @NotBlank
     private String beerName;
 
+    @NotNull
     private BeerStyleEnum beerStyle;
 
+    @Positive
+    @NotNull
     private Long upc;
 
     private BigDecimal price;
