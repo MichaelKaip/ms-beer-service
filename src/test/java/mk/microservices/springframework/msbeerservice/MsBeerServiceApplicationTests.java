e63a7f1241d68bad21117c69db827fd0f63274c4
Michael Kaip <michael.kaip@student.htw-berlin.de>
2020-08-18 13:59:20 +0200

    Adds the initial project. Closes #1.

---
+++ src/test/java/mk/microservices/springframework/msbeerservice/MsBeerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package mk.microservices.springframework.msbeerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsBeerServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
