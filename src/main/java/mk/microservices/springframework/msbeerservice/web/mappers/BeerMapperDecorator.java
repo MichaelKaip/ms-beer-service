acbc4e847e1c98a9d830ca34ea3a9fa991d9683b
Michael Kaip <michael.kaip@student.htw-berlin.de>
2020-08-20 13:33:44 +0200

    Updates the project to inherit dependencies from BOM. Closes #23.

--- src/main/java/mk/microservices/springframework/msbeerservice/web/mappers/BeerMapperDecorator.java
+++ src/main/java/mk/microservices/springframework/msbeerservice/web/mappers/BeerMapperDecorator.java
@@ -6,6 +6,7 @@ import mk.microservices.springframework.msbeerservice.web.model.BeerDto;
 import org.springframework.beans.factory.annotation.Autowired;
 
 public abstract class BeerMapperDecorator implements BeerMapper {
+
     private BeerInventoryService beerInventoryService;
     private BeerMapper mapper;
 
