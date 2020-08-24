22992fdf5fc8f564b3d5933142bbde7d4a5955ed
Michael Kaip <michael.kaip@student.htw-berlin.de>
2020-08-24 09:31:23 +0200

    Fixes the Hibernate-Errors on Startup with MySQL. Closes #27.

--- target/generated-sources/annotations/mk/microservices/springframework/msbeerservice/web/mappers/BeerMapperImpl.java
+++ target/generated-sources/annotations/mk/microservices/springframework/msbeerservice/web/mappers/BeerMapperImpl.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2020-08-20T18:05:35+0200",
+    date = "2020-08-21T17:57:56+0200",
     comments = "version: 1.3.1.Final, compiler: javac, environment: Java 11.0.7 (Oracle Corporation)"
 )
 @Component
